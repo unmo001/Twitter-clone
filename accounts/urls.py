0ec22bf6228fbb5791c23f25fc0645a0b7aecfc4
unmo0 <unmo001@gmaill.com>
2020-12-08 01:46:19 +0900

    login実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = "accounts"
 
 urlpatterns = [
-    path('', views.TopPageView.as_view(), name='TopPage'),
+    path('', views.TopPageView.as_view(), name='toppage'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="TopPage.html"), name='logout'),
+    path('index/', views.IndexView.as_view(), name='index')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
-
 # Create your views here.
+from accounts.forms import SignUpForm
+
+
 class TopPageView(TemplateView):
     template_name = 'TopPage.html'
 
 
 toppageview = TopPageView.as_view()
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+indexview = IndexView.as_view()
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
